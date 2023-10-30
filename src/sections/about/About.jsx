8093b8ccb42398fa28673445747a4d0c4e24cfd4
Rohit <iamrohit3000@gmail.com>
2023-10-30 12:47:30 +0530

    fixed some typos and updates some docs

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,17 +1,12 @@
-import React , {useEffect, useState} from 'react';
-import AboutImage from '../../assests/about.jpg'
-import {HiDownload} from 'react-icons/hi'
-import CV from "../../assests/Rohit sharma-Resume.pdf"
+import React, { useEffect, useState } from "react";
+import AboutImage from "../../assests/about.jpg";
+import { HiDownload } from "react-icons/hi";
+import CV from "../../assests/Rohit sharma-Resume.pdf";
 import "./about.css";
 
-
-const About = ({theme}) => {
-
-
+const About = ({ theme }) => {
   return (
-    
-    <section id="about" data-aos ="fade-in"  className={`${theme}`}>
-  
+    <section id="about" data-aos="fade-in" className={`${theme}`}>
       <div className="container about__container">
         <div className="about__left">
           <div className="about__portrait">
@@ -21,16 +16,25 @@ const About = ({theme}) => {
         <div className="about__right">
           <h2>About Me</h2>
           <p>
-          Hi, I'm Rohit Sharma, a third-year B.Tech student with a CGPA of 9.00 as of the 4th semester and interested in web development and competitive programming. 
+            Hi, I'm Rohit Sharma, a third-year B.Tech student with a CGPA of
+            9.00 as of the 4th semester and interested in web development and
+            competitive programming.
           </p>
           <p>
-          I am  proficient in a variety of web development technologies [MERN] and along with that , I have a max rating of 1540 on CodeChef. <br /><br />
-          In my spare time, I enjoy  playing cricket , games , and spending time with my friends.
+            I am proficient in a variety of web development technologies [MERN]
+            and along with that , I have a max rating of 1540 on CodeChef.{" "}
+            <br />
+            <br />
+            In my spare time, I enjoy playing cricket , games , and spending
+            time with my friends.
             <br /> <br />
-          Thank you for taking the time to visit my website, and I look forward to connecting with you.
+            Thank you for taking the time to visit my website, and I look
+            forward to connecting with you.
           </p>
-          
-          <a href={CV} download className='btn primary'>Download cv <HiDownload/></a>
+
+          <a href={CV} download className="btn primary">
+            Download cv <HiDownload />
+          </a>
         </div>
       </div>
     </section>
--- src/sections/services/data.js
+++ src/sections/services/data.js
@@ -6,10 +6,10 @@ import {BsMotherboardFill} from 'react-icons/bs'
 const data = [
   {id: 1 , icon: <RiReactjsLine/> , title: 'Frontend Development' , desc: "HTML , CSS , Bootstrap ,  javaScript , React"} , 
 
-  {id: 2 , icon: <FaServer/> , title: 'Backend Development' , desc: "Nodejs , Expressjs , MongoDB , SQL ,"} , 
+  {id: 2 , icon: <AiFillCreditCard/> , title: 'Data Structure and Algorithm' , desc: " Using java"} ,
+  {id: 3 , icon: <BsMotherboardFill/> , title: 'Version Control' , desc: "Git , Github "} ,
+  
+  {id: 4 , icon: <FaServer/> , title: 'Others' , desc: "communication , video editing "} , 
 
-  {id: 3 , icon: <AiFillCreditCard/> , title: 'Data Structure and Algorithm' , desc: " Using java"} ,
-  {id: 3 , icon: <BsMotherboardFill/> , title: 'Others' , desc: "Git , Github , Video Editing"} ,
 ]
-
 export default data;
