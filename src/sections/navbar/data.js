5493dc8e6eac1c1f1b5f9fedb72dbac89c6b4fa5
Rohit <iamrohit3000@gmail.com>
2023-12-03 17:23:31 +0530

    added hyperlink and removed the project bug

--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -2,7 +2,7 @@ const data = [
   {id: 1 , link: '#' , title: 'Home'},
   {id: 2 , link: '#about' , title: 'About'},
   {id: 3 , link: '#skills' , title: 'Skills'},
-  {id: 4 , link: '#projects' , title: 'Projects'},
+  {id: 4 , link: '#project' , title: 'Projects'},
   {id: 5 , link: '#contact' , title: 'Contact'},
 ]
 
