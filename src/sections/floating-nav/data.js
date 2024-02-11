0ef0e806241974fcd9add5204d69c9be92465348
Rohit <iamrohit3000@gmail.com>
2024-02-12 01:56:37 +0530

    added flaoting nav-bar experience icon

--- src/sections/floating-nav/data.js
+++ src/sections/floating-nav/data.js
@@ -3,13 +3,15 @@ import { BsPersonFill } from "react-icons/bs";
 import { GiSkills } from "react-icons/gi";
 import { AiFillProject } from "react-icons/ai";
 import { AiFillMessage } from "react-icons/ai";
+import { FaCode } from "react-icons/fa";
 
 const data = [
   { id: "1", link: "#", icon: <AiFillHome /> },
   { id: "2", link: "#about", icon: <BsPersonFill /> },
-  { id: "3", link: "#skills", icon: <GiSkills /> },
-  { id: "4", link: "#project", icon: <AiFillProject /> },
-  { id: "5", link: "#contact", icon: <AiFillMessage /> },
+  { id: "2", link: "#Exp", icon: <FaCode /> },
+  { id: "4", link: "#skills", icon: <GiSkills /> },
+  { id: "5", link: "#project", icon: <AiFillProject /> },
+  { id: "6", link: "#contact", icon: <AiFillMessage /> },
 ];
 
 export default data;
--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -1,9 +1,10 @@
 const data = [
-  {id: 1 , link: '#' , title: 'Home'},
-  {id: 2 , link: '#about' , title: 'About'},
-  {id: 3 , link: '#skills' , title: 'Skills'},
-  {id: 4 , link: '#project' , title: 'Projects'},
-  {id: 5 , link: '#contact' , title: 'Contact'},
-]
+  { id: 1, link: "#", title: "Home" },
+  { id: 2, link: "#about", title: "About" },
+  { id: 2, link: "#Exp", title: "Experience" },
+  { id: 4, link: "#skills", title: "Skills" },
+  { id: 5, link: "#project", title: "Projects" },
+  { id: 6, link: "#contact", title: "Contact" },
+];
 
 export default data;
